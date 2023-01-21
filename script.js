7e578e0d1c506c6d2229fd9601ee8397fca43d5a
Liya <82108363+Slyrith@users.noreply.github.com>
2023-01-21 06:41:52 +0000

    updated

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ var i = 0;
 var txt = 'Login to access E7'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+document.addEventListener("DOMContentLoaded", function(){
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("E7T").textContent += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-}
+  typeWriter();
+  
+  // Add event listener to the toptext element
+  var timeoutId;
+  document.getElementById("E7T").addEventListener("mouseover", function() {
+    document.querySelector(".hidden").style.display = "inline";
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function(){
+      document.querySelector(".hidden").style.opacity = "0";
+    }, 2000);
+  });
+  document.getElementById("E7T").addEventListener("mouseout", function() {
+    setTimeout(function(){
+      document.querySelector(".hidden").style.display = "none";
+    }, 2000);
+  });
+});
